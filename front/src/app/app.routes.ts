977dd1c02e26745f8e8f50f4af0a7536ac4be141
PedrOliveiraM <pedropucmont@gmail.com>
2024-12-04 10:37:45 -0300

    feat: implement routing and add books, authors, and categories components with tests

--- front/src/app/app.routes.ts
+++ front/src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
+import { BooksComponent } from './books/books.component';
+import { AuthorsComponent } from './authors/authors.component';
+import { CategoriesComponent } from './categories/categories.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: 'books', component: BooksComponent },
+  { path: 'authors', component: AuthorsComponent },
+  { path: 'categories', component: CategoriesComponent },
+];
